c8cd74e2dfb54e196cc66ba887f7a24476e9a34f
catcarbonell <hello@catcarbn.com>
2020-06-16 21:53:18 -0700

    changed siteURL in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
       summary: `a UX/UI Dev who runs on	🍜noodles.`,
     },
     description: `A blog containing Cat's learnings, thoughts, feels, and (occasionally) drawings.`,
-    siteUrl: `https://suspicious-bhaskara-5431cf.netlify.app/`,
+    siteUrl: `https://blog.catcarbonell.dev/`,
     social: {
       twitter: `catcarbn`,
     },
